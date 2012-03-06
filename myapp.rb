e23fa82d451f1bc2d31720a3e2beb6f8761d5218
Michael Rose <elementation@gmail.com>
2012-03-05 21:16:02 -0700

    Initial commit of webhook tester

---
+++ myapp.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'json'
+require 'pp'
+
+get '/webhook' do
+  "Hello Webhook"
+end
+
+post '/webhook' do
+  pp "Receiving Webhook #{params[:webhookId]}"
+  pp JSON.parse(params[:result])
+end
